a9ab3140b7aec8f76a3dffab0a9aed2c4bfdedba
koseyeon <koseyeon97@gmail.com>
2022-03-13 20:20:56 +0900

    body.style.backgroundImage를 변경하도록 구현

--- js/backgroud.js
+++ js/backgroud.js
@@ -2,8 +2,6 @@ const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg"]
 const imgPath = "img";
 const todayImage = images[Math.floor((Math.random()*images.length))]
 function paintBackground(){
-  const bgImg = document.createElement("img");
-  bgImg.src = `${imgPath}/${todayImage}`;
-  document.body.appendChild(bgImg);
+  document.body.style.backgroundImage = `url('${imgPath}/${todayImage}')`
 }
 paintBackground();
